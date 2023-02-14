36148ebf4971dc1c193a891e71bfdd1c4a989d33
Phuc Hoang <>
2023-02-14 18:22:32 +0700

    Implement DFS

--- graph.py
+++ graph.py
@@ -6,14 +6,14 @@ class Graph:
         self.numVertices = 0
 
 
-    def addVertex(self,key):
+    def addVertex(self, key):
         self.numVertices = self.numVertices + 1
         newVertex = Vertex(key)
         self.vertList[key] = newVertex
         return newVertex
 
 
-    def getVertex(self,n):
+    def getVertex(self, n):
         if n in self.vertList:
             return self.vertList[n]
         else:
@@ -35,6 +35,42 @@ class Graph:
     def getVertices(self):
         return self.vertList.keys()
 
-
     def __iter__(self):
         return iter(self.vertList.values())
+    
+class DFSGraph(Graph):
+    def __init__(self):
+        super().__init__()
+        self.time = 0
+
+    def dfs(self):
+        for aVertex in self:
+            aVertex.setColor('white')
+            aVertex.setPred(None)
+        for aVertex in self:
+            if aVertex.getColor() == 'white':
+                self.dfsvisit(aVertex)
+
+    def dfsvisit(self, startVertex):
+        startVertex.setColor('gray')
+        self.time += 1
+        startVertex.setDiscovery(self.time)
+        for nextVertex in startVertex.getConnections():
+            if nextVertex.getColor() == 'white':
+                nextVertex.setPred(startVertex)
+                self.dfsvisit(nextVertex)
+        self.time += 1
+        startVertex.setFinish(self.time)
+
+    def traverse(self, y):
+        x = y
+        trace = []
+        while (x.getPred()):
+            trace.append(x)
+            x = x.getPred()
+            if(x.getPred() == None):
+                trace.append(x)
+
+        trace = reversed(trace)
+        result = "->".join(str(v.getId()) for v in trace)
+        print(result)
--- vertex.py
+++ vertex.py
@@ -2,6 +2,10 @@ class Vertex:
     def __init__(self,key):
         self.id = key
         self.connectedTo = {}
+        self.time = -1
+        self.finish = -1
+        self.pred = None
+        self.color = None
 
 
     def addNeighbor(self,nbr,weight=0):
@@ -9,7 +13,10 @@ class Vertex:
 
 
     def __str__(self):
-        return str(self.id) + ' connectedTo: ' + str(x.id for x in self.connectedTo)
+        return str(self.id)
+
+    def __repr__(self) -> str:
+        return str(self.id)
 
 
     def getConnections(self):
@@ -22,5 +29,21 @@ class Vertex:
 
     def getWeight(self,nbr):
         return self.connectedTo[nbr]
-
     
+    def setDiscovery(self, time):
+        self.time = time
+
+    def setFinish(self, finish):
+        self.finish = finish
+
+    def setPred(self, pred):
+        self.pred = pred
+
+    def setColor(self, color):
+        self.color = color 
+
+    def getColor(self):
+        return self.color
+
+    def getPred(self):
+        return self.pred   
